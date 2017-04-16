223cf2888960de316f167b078092d0ebd8af32ff
dmx <dmx1988@126.com>
2017-04-16 03:50:17 -0700

    调整目录结构

---
+++ hubot_test/test.js
@@ -0,0 +1,22 @@
+module.exports = function(robot) {
+    robot.respond(/is it (weekend|holiday)\s?\?/i, function(msg){
+        var today = new Date();
+
+        var con = new ActiveXObject("ADODB.Connection");
+		con.ConnectionString = "DRIVER={MySQL ODBC 5.1 Driver};OPTION=3;SERVER=127.0.0.1;User ID=root;Password=123456;Database=mysql;Port=3306";  
+		con.open;
+		var rs = new ActiveXObject("ADODB.Recordset");
+		rs.open("select * from user", con);
+		while (!rs.eof) {
+			var u = rs.Fields("User");
+			document.write(u);
+			rs.moveNext;
+		}
+		rs.close();
+		rs = null; 
+		con.close();
+		con = null;
+
+        msg.reply(today.getDay() === 0 || today.getDay() === 6 ? "YES" : "NO");
+    });
+}
